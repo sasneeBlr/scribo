17a6fadbc79cbe90074c1d71be91c1ef20016e21
Sasnee <sasnee.tech@gmail.com>
2016-03-24 15:38:02 +0530

    First version of scribo library along with sample application.
    scribo enables users to route the debug logs to the ADB console
    as well as write it to a file on the SDCard for later use. The
    DebugHelper class in the scribo library provides various types
    of functions to achieve the following:
    - Set the name of the file in which the logs need to be recorded.
    - Override the default log categories provided by scribo with the
      ones which are more relevant for the users.
    - Enable/Disable any of the log categories dynamically. (Except
      GENERAL all other category logs are disabled by default)
    
    A simple sample application is provided to demonstrate the usage
    of scribo.

---
+++ app/src/main/java/com/sasnee/scribosample/ScriboSample.java
@@ -0,0 +1,90 @@
+package com.sasnee.scribosample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.sasnee.scribo.DebugHelper;
+
+public class ScriboSample extends Activity {
+    private static final String TAG = "ScriboSample";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dhsample);
+
+        /* Initialize the library */
+        DebugHelper.init(this);
+
+        Log.i(TAG, "onResume: Journal file: " + DebugHelper.getFilePath());
+
+        /* Map default categories to custom log masks. */
+        DebugHelper.mapCustomLogMask(DebugHelper.LOG_CATEGORY_0, "CATEGORY 0");
+        DebugHelper.mapCustomLogMask(DebugHelper.LOG_CATEGORY_5, "CATEGORY 5");
+        DebugHelper.mapCustomLogMask(DebugHelper.LOG_CATEGORY_7, "CATEGORY 7");
+        DebugHelper.mapCustomLogMask(DebugHelper.LOG_CATEGORY_10, "CATEGORY 10");
+
+        /* Enable/Disable the log category either based on the default categories... */
+        DebugHelper.enableDisableLogCategory(DebugHelper.LOG_CATEGORY_4, true);
+        DebugHelper.enableDisableLogCategory(DebugHelper.LOG_CATEGORY_8, false);
+        DebugHelper.enableDisableLogCategory(DebugHelper.LOG_CATEGORY_9, true);
+
+        /* ...or based on the custom log masks defined above... */
+        DebugHelper.enableDisableLogCategory("CATEGORY 0", true);
+        DebugHelper.enableDisableLogCategory("CATEGORY 5", false);
+        DebugHelper.enableDisableLogCategory("CATEGORY 7", true);
+
+        /* Even if you have mapped default category to a custom log mask, you can still
+         * use the default category defined in the library. */
+        DebugHelper.enableDisableLogCategory(DebugHelper.LOG_CATEGORY_10, true);
+
+
+        /* Now, use any of the below listed variants of the API
+         * DebugHelper.logRequest() to pass the log string to the library */
+
+        /* Variant 1 */
+        DebugHelper.logRequest(TAG, "Variant type 1");
+
+        /* Variant 2 */
+        DebugHelper.logRequest(TAG, "Variant type 2", true/* Show on ADB logs */);
+
+        /* Variant 3 */
+        DebugHelper.logRequest(TAG, "Variant type 3", DebugHelper.SEVERITY_LEVEL_INFO);
+
+        /* Variant 4 */
+        DebugHelper.logRequest(TAG, "Variant type 4", "CATEGORY 5");
+
+        /* Variant 5 */
+        DebugHelper.logRequest(TAG, "Variant type 5", DebugHelper.LOG_CATEGORY_4);
+
+        /* Variant 6 */
+        DebugHelper.logRequest(TAG, "Variant type 6",
+                                true /* Show on ADB logs */, DebugHelper.SEVERITY_LEVEL_VERBOSE);
+
+        /* Variant 7 */
+        DebugHelper.logRequest(TAG, "Variant type 7",
+                                false /* Show on ADB logs */, "CATEGORY 5");
+
+        /* Variant 8 */
+        DebugHelper.logRequest(TAG, "Variant type 8",
+                                false /* Show on ADB logs */, DebugHelper.LOG_CATEGORY_9);
+
+        /* Variant 9 */
+        DebugHelper.logRequest(TAG, "Variant type 9",
+                                DebugHelper.SEVERITY_LEVEL_INFO, "CATEGORY 7");
+
+        /* Variant 10 */
+        DebugHelper.logRequest(TAG, "Variant type 10",
+                                DebugHelper.SEVERITY_LEVEL_VERBOSE, DebugHelper.LOG_CATEGORY_7);
+
+        /* Variant 11 */
+        DebugHelper.logRequest(TAG, "Variant type 11", true /* Show on ADB logs */,
+                                DebugHelper.SEVERITY_LEVEL_WARN, "CATEGORY 10");
+
+        /* Variant 12 */
+        DebugHelper.logRequest(TAG, "Variant type 12", true /* Show on ADB logs */,
+                                DebugHelper.SEVERITY_LEVEL_WARN, DebugHelper.LOG_CATEGORY_GENERAL);
+
+    }
+}
---
+++ scribo/src/main/java/com/sasnee/scribo/DebugHelper.java
@@ -0,0 +1,340 @@
+package com.sasnee.scribo;
+
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class DebugHelper {
+
+    private static final String TAG = "DebugHelper";
+
+    private static FileOutputStream mOutput = null;
+    private static OutputStreamWriter mOutputStreamWriter = null;
+
+    public static String filePath =
+            Environment.getExternalStorageDirectory().getAbsolutePath().toString();
+
+    private static final String DEFAULT_JOURNAL_FILE = "logJournal.txt";
+    private static final Boolean DEFAULT_RESET_OPTION = true; // Reset file contents everytime.
+
+    private static String mLogJournalFile = filePath + DEFAULT_JOURNAL_FILE;
+    private File mFile;
+    private Context mContext;
+
+    /* DebugHelper class is singleton */
+    private static DebugHelper sInstance = null;
+
+    public final static int SEVERITY_LEVEL_ERROR    = 0x1;
+    public final static int SEVERITY_LEVEL_WARN     = 0x2;
+    public final static int SEVERITY_LEVEL_VERBOSE  = 0x4;
+    public final static int SEVERITY_LEVEL_INFO     = 0x8;
+
+    public final static long LOG_CATEGORY_0         = 0x1;
+    public final static long LOG_CATEGORY_1         = 0x2;
+    public final static long LOG_CATEGORY_2         = 0x4;
+    public final static long LOG_CATEGORY_3         = 0x8;
+    public final static long LOG_CATEGORY_4         = 0x10;
+    public final static long LOG_CATEGORY_5         = 0x20;
+    public final static long LOG_CATEGORY_6         = 0x40;
+    public final static long LOG_CATEGORY_7         = 0x80;
+    public final static long LOG_CATEGORY_8         = 0x100;
+    public final static long LOG_CATEGORY_9         = 0x200;
+    public final static long LOG_CATEGORY_10        = 0x400;
+    public final static long LOG_CATEGORY_GENERAL   = 0x800;
+
+    private final static boolean DEFAULT_ADB_BEHAVIOUR = true;
+    private final static int DEFAULT_SEVERITY_LEVEL = SEVERITY_LEVEL_VERBOSE;
+    private final static long DEFAULT_LOG_CATEGORY_MASK = LOG_CATEGORY_GENERAL;
+    private static long logCategoryMask = LOG_CATEGORY_GENERAL;
+
+    private static HashMap<Long, String>LogMaskLookupTable;
+
+    private DebugHelper(Context context) {
+        mContext = context;
+    }
+
+    public static void init(Context context) {
+        init(context, DEFAULT_JOURNAL_FILE, DEFAULT_RESET_OPTION);
+    }
+
+    public static void init(Context context, String fileName) {
+        init(context, fileName, DEFAULT_RESET_OPTION);
+    }
+
+    public static void init(Context context, Boolean resetFileContents) {
+        init(context, DEFAULT_JOURNAL_FILE, resetFileContents);
+    }
+
+    public static void init(Context context, String fileName, Boolean resetFileContents) {
+        if (sInstance == null) {
+            synchronized (DebugHelper.class) {
+                if (sInstance == null) {
+                    sInstance = new DebugHelper(context);
+                }
+            }
+        }
+
+        if (!fileName.contains(".txt")) {
+            // Append .txt to the file name to make life simpler!
+            fileName = fileName + ".txt";
+        }
+
+        sInstance.setLogJournalFile(fileName);
+
+        if (resetFileContents) {
+            // Reset the file contents.
+            startFromScratch();
+        }
+
+        prePopulateLookupTable();
+    }
+
+    private void setLogJournalFile(String fileName) {
+        filePath = filePath + "/AppData/" + mContext.getApplicationInfo().processName + "/";
+        mLogJournalFile = filePath + fileName;
+
+        mFile = new File(filePath);
+        mFile.mkdirs();
+
+        Log.i(TAG, "setLogJournalFile: Journal file = " + mLogJournalFile);
+    }
+
+    private static void prePopulateLookupTable() {
+        LogMaskLookupTable = new HashMap<>();
+
+        LogMaskLookupTable.put(LOG_CATEGORY_0,  "Log Category 0");
+        LogMaskLookupTable.put(LOG_CATEGORY_1,  "Log Category 1");
+        LogMaskLookupTable.put(LOG_CATEGORY_2,  "Log Category 2");
+        LogMaskLookupTable.put(LOG_CATEGORY_3,  "Log Category 3");
+        LogMaskLookupTable.put(LOG_CATEGORY_4,  "Log Category 4");
+        LogMaskLookupTable.put(LOG_CATEGORY_5,  "Log Category 5");
+        LogMaskLookupTable.put(LOG_CATEGORY_6,  "Log Category 6");
+        LogMaskLookupTable.put(LOG_CATEGORY_7,  "Log Category 7");
+        LogMaskLookupTable.put(LOG_CATEGORY_8,  "Log Category 8");
+        LogMaskLookupTable.put(LOG_CATEGORY_9, "Log Category 9");
+        LogMaskLookupTable.put(LOG_CATEGORY_10, "Log Category 10");
+    }
+
+    public static String getFilePath() {
+        return mLogJournalFile;
+    }
+
+    private static void startFromScratch() {
+        // Just open close the file in non-append mode to reset the contents.
+        try {
+            PrintWriter pw = new PrintWriter(mLogJournalFile);
+            pw.close();
+        } catch (Exception e) {
+            Log.e(TAG, "DebugHelper : Exception: " + e.getCause() + " | " + e.getMessage());
+        }
+    }
+
+    public static boolean mapCustomLogMask(long defaultCategoryMask, String customLogMask) {
+        boolean result = true;
+
+        if (defaultCategoryMask >= LOG_CATEGORY_0 && defaultCategoryMask <= LOG_CATEGORY_10)
+            LogMaskLookupTable.put(defaultCategoryMask, customLogMask);
+        else
+            result = false;
+
+        return result;
+    }
+
+    private static long getLogCategoryFromCustomLogMask(String customLogMask) {
+        long returnVal = -1;
+
+        for (Long keyItem : LogMaskLookupTable.keySet()) {
+            if (LogMaskLookupTable.get(keyItem).equals(customLogMask)) {
+                returnVal = keyItem;
+                break;
+            }
+        }
+
+        return returnVal;
+    }
+
+    public static void enableDisableLogCategory(long category, boolean isEnable) {
+        if (isEnable) {
+            logCategoryMask = logCategoryMask | category;
+        } else {
+            logCategoryMask = logCategoryMask & ~category;
+        }
+    }
+
+    public static void enableDisableLogCategory(String customLogMask, boolean isEnable) {
+
+        long category = LOG_CATEGORY_GENERAL;
+        boolean customLogMaskFound = false;
+
+        for (Long keyItem : LogMaskLookupTable.keySet()) {
+            if (LogMaskLookupTable.get(keyItem).equals(customLogMask)) {
+                category = keyItem;
+                customLogMaskFound = true;
+                break;
+            }
+        }
+        if (customLogMaskFound) {
+            enableDisableLogCategory(category, isEnable);
+        } else {
+            Log.i(TAG, "enableDisableLogCategory: Unrecognized custom log mask: " + customLogMask);
+        }
+    }
+
+
+    private static String logFunction(int severity, String tag, String logEntry,
+                                      boolean shouldOutputToADB) {
+        String severityLevel = null;
+
+        switch (severity) {
+            case SEVERITY_LEVEL_ERROR:
+                severityLevel = "Error";
+                Log.e(tag, logEntry);
+                break;
+            case SEVERITY_LEVEL_WARN:
+                severityLevel = "Warning";
+                if (shouldOutputToADB) {
+                    Log.w(tag, logEntry);
+                }
+                break;
+            case SEVERITY_LEVEL_VERBOSE:
+                severityLevel = "Verbose";
+                if (shouldOutputToADB) {
+                    Log.v(tag, logEntry);
+                }
+                break;
+            case SEVERITY_LEVEL_INFO:
+                severityLevel = "Info";
+                if (shouldOutputToADB) {
+                    Log.i(tag, logEntry);
+                }
+                break;
+            default:
+                severityLevel = "Verbose";
+                if (shouldOutputToADB) {
+                    Log.v(tag, logEntry);
+                }
+                break;
+        }
+
+        return severityLevel;
+    }
+
+    synchronized private static void updateLogJournal(String tag, String logEntry,
+                                                      String severityLevel) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
+        String currentDateTime = sdf.format(new Date());
+
+        try {
+            mOutput = new FileOutputStream(mLogJournalFile, true);
+            mOutputStreamWriter = new OutputStreamWriter(mOutput);
+            mOutputStreamWriter.append(currentDateTime + ": " + tag + " |"
+                                        + severityLevel + "|" + ": ");
+            mOutputStreamWriter.append(logEntry);
+            mOutputStreamWriter.append("\n");
+            mOutputStreamWriter.flush();
+            mOutputStreamWriter.close();
+            mOutput.close();
+        } catch (Exception e) {
+            Log.e(TAG, "updateLogJournal : Exception: " + e.getCause() + "|" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void logRequest(String tag, String logEntry) {
+        logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, DEFAULT_SEVERITY_LEVEL,
+                    DEFAULT_LOG_CATEGORY_MASK);
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs) {
+        logRequest(tag, logEntry, showOnADBLogs, DEFAULT_SEVERITY_LEVEL, DEFAULT_LOG_CATEGORY_MASK);
+    }
+
+    public static void logRequest(String tag, String logEntry, int severity) {
+        logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, severity, DEFAULT_LOG_CATEGORY_MASK);
+    }
+
+    public static void logRequest(String tag, String logEntry, String customLogMask) {
+        long category = getLogCategoryFromCustomLogMask(customLogMask);
+        if (category >= 0) {
+            logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, DEFAULT_SEVERITY_LEVEL, category);
+        } else {
+            Log.i(TAG, "logRequest: Unrecognized custom log mask : " + customLogMask);
+        }
+    }
+
+    public static void logRequest(String tag, String logEntry, long category) {
+        logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, DEFAULT_SEVERITY_LEVEL, category);
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs,
+                                  int severity) {
+        logRequest(tag, logEntry, showOnADBLogs, severity, DEFAULT_LOG_CATEGORY_MASK);
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs,
+                                  String customLogMask) {
+        long category = getLogCategoryFromCustomLogMask(customLogMask);
+        if (category >= 0) {
+            logRequest(tag, logEntry, showOnADBLogs, DEFAULT_SEVERITY_LEVEL, category);
+        } else {
+            Log.i(TAG, "logRequest: Unrecognized custom log mask : " + customLogMask);
+        }
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs,
+                                  long category) {
+        logRequest(tag, logEntry, showOnADBLogs, DEFAULT_SEVERITY_LEVEL, category);
+    }
+
+    public static void logRequest(String tag, String logEntry, int severity, String customLogMask) {
+        long category = getLogCategoryFromCustomLogMask(customLogMask);
+        if (category >= 0) {
+            logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, severity, category);
+        } else {
+            Log.i(TAG, "logRequest: Unrecognized custom log mask : " + customLogMask);
+        }
+    }
+
+    public static void logRequest(String tag, String logEntry, int severity, long category) {
+        logRequest(tag, logEntry, DEFAULT_ADB_BEHAVIOUR, severity, category);
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs,
+                                  int severity, String customLogMask) {
+        long category = getLogCategoryFromCustomLogMask(customLogMask);
+        if (category >= 0) {
+            logRequest(tag, logEntry, showOnADBLogs, severity, category);
+        } else {
+            Log.i(TAG, "logRequest: Unrecognized custom log mask : " + customLogMask);
+        }
+    }
+
+    public static void logRequest(String tag, String logEntry, boolean showOnADBLogs,
+                                  int severity, long category) {
+        String severityLevel;
+
+        boolean shouldOutputToADB = showOnADBLogs && ((category & logCategoryMask) > 0);
+
+        if (tag == null) {
+            tag = TAG;
+        }
+
+        severityLevel = logFunction(severity, tag, logEntry, shouldOutputToADB);
+
+        // Write to journal file only if the logCategoryMask allows it.
+        if ((category & logCategoryMask) > 0) {
+            String state = android.os.Environment.getExternalStorageState();
+            if (state.equals(android.os.Environment.MEDIA_MOUNTED))
+                updateLogJournal(tag, logEntry, severityLevel);
+        }
+    }
+}
